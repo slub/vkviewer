d0071bbe9c3ccbd2d77243428593fbc9a5465aa4
Jacob Mendt <jacobmendt@googlemail.com>
2014-03-04 11:17:13 +0100

    Update db privilieges for user vkviewer

--- env/db/db_privilieges.sql
+++ env/db/db_privilieges.sql
@@ -1,1 +1,15 @@
-﻿GRANT SELECT ON spatial_ref_sys TO vkviewer
+﻿GRANT SELECT ON spatial_ref_sys TO vkviewer;
+GRANT SELECT ON TABLE webmappingservice TO vkviewer;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE users TO vkviewer;
+GRANT SELECT ON TABLE refmtbwms TO vkviewer;
+GRANT SELECT ON TABLE spatial_ref_sys TO vkviewer;
+GRANT SELECT, UPDATE ON TABLE messtischblatt TO vkviewer;
+GRANT SELECT ON TABLE md_zeit TO vkviewer;
+GRANT SELECT ON TABLE md_core TO vkviewer;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE georeferenzierungsprozess TO vkviewer;
+GRANT SELECT, INSERT ON TABLE fehlermeldungen TO vkviewer;
+GRANT ALL ON TABLE users_id_seq TO vkviewer;
+GRANT ALL ON TABLE georeferenzierungsprozess_id_seq TO vkviewer;
+GRANT ALL ON TABLE fehlermeldungen_id_seq TO vkviewer;
+
+
