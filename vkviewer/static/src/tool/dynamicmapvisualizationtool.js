33c293b13a80d670894dcafd6bed492bfd051fac
Jacob Mendt <jacobmendt@googlemail.com>
2014-10-28 17:05:36 +0100

    Add vk2.tool.DynamicMapVisualization tool

---
+++ vkviewer/static/src/tool/dynamicmapvisualizationtool.js
@@ -0,0 +1,10 @@
+goog.provide('vk2.tool.DynamicMapVisualization');
+
+/**
+ * @constructor
+ */
+vk2.tool.DynamicMapVisualization = function(){
+	
+	if (goog.DEBUG)
+		console.log('Initialize vk2.tool.DynamicMapVisualization ...');
+};
